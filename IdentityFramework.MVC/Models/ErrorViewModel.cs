e87c7114e98bd0e2b32bff4fad415ca7563da0be
Cauã Jordão <153930450+cauaojordao@users.noreply.github.com>
2025-02-28 19:27:12 -0300

    Delete IdentityFramework.MVC directory

--- IdentityFramework.MVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace IdentityFramework.MVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
